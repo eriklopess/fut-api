70d41ff4d02a10a71e1260aa77ff61e3cc3555cb
Erik Lopes de Oliveira <erik.lopesoliveira2019@gmail.com>
2024-03-09 13:17:41 -0300

    config: install fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyHelmet from '@fastify/helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.register(fastifyHelmet);
   await app.listen(3000);
 }
 bootstrap();
