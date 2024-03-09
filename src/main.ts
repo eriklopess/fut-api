05d65f82e80897f0299ef60bdf694494f865bc95
Erik Lopes de Oliveira <erik.lopesoliveira2019@gmail.com>
2024-03-09 14:49:24 -0300

    config: add rate limiting and logger

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,18 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import fastifyHelmet from '@fastify/helmet';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from './shared/config/logger.config';
 
 async function bootstrap() {
+  const fastifyAdapter = new FastifyAdapter();
+  const logger = WinstonModule.createLogger(winstonConfig);
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    fastifyAdapter,
+    {
+      logger,
+    },
   );
   await app.register(fastifyHelmet);
   await app.listen(3000);
---
+++ src/shared/interceptors/Logger.Interceptor.ts
@@ -0,0 +1,35 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Logger } from 'winston';
+
+@Injectable()
+export class LoggerInterceptor implements NestInterceptor {
+  constructor(@Inject('winston') private logger: Logger) {}
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    this.log(context.switchToHttp().getRequest());
+    return next.handle();
+  }
+
+  private log(req) {
+    const body = { ...req.body };
+    delete body.password;
+    delete body.passwordConfirmation;
+    this.logger.info({
+      timestamp: new Date().toISOString(),
+      method: req.method,
+      route: req.route.path,
+      data: {
+        body: body,
+        query: req.query,
+        params: req.params,
+      },
+      from: req.ip,
+    });
+  }
+}
