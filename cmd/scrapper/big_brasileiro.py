fcfa6a596ab543cb366b0bc562950f1a3924ccfb
m-carneiro <m.carneiro.tech@gmail.com>
2024-03-14 15:53:25 -0300

    feat: add Scrapper files, update table and rounds functions, remove unused code

--- cmd/scrapper/big_brasileiro.py
+++ cmd/scrapper/big_brasileiro.py
@@ -1,12 +1,11 @@
 import requests
 import json
+import os
+import uuid
 from bs4 import BeautifulSoup
+from utils import today, create_file
 
 url = 'https://www.futebolinterior.com.br/campeonato/brasileirao-serie-a-2024/'
-
-def create_file(text: str, page_name: str, extension: str):
-    with open(f'{page_name}.{extension}', 'w') as file:
-        file.write(text)
 
 def download_page(url):
     response = requests.get(url)
@@ -18,10 +17,10 @@ def get_data():
     soup = BeautifulSoup(str(response), 'html.parser')
     
     table = soup.find('table', class_='table-classification--expansive')
-    print(get_table(table))
+    get_table(table)
     
     rounds = soup.find('div', class_='rounds')
-    print(get_rounds(rounds))
+    get_rounds(rounds)
     
         
         
@@ -49,6 +48,7 @@ def get_rounds(rounds) -> list:
     matches = []
     for i in range(len(round_teams)):
         match = {
+            'id': str(uuid.uuid4()),
             'date': utils_date(dates[int(i / 2)]),
             'teams': f'{round_teams[i - 1]} x {round_teams[i]}'
         }
@@ -56,11 +56,63 @@ def get_rounds(rounds) -> list:
         
     del matches[0::2]
     jsonify = json.dumps(matches, indent=4)
-    create_file(str(jsonify), 'matches', 'json')
+    
+    create_file(str(jsonify), f'rodada_{today()}', 'json')
     return matches
 
 def get_table(table) -> list:
-    pass
+    table = table.find_all('tr')
+    tabela = {}
+    teams = []
+    for i in table:
+        teams.append(i.text)
+    
+    for j in range(len(teams)):
+        teams[j] = teams[j].replace('\n', '-').strip()
+        teams[j] = teams[j].replace(' ', '').strip()
+        teams[j] = teams[j].replace('\r', '').strip()
+    
+    for k in range(len(teams)):
+        teams[k] = teams[k].split('-')
+        teams[k] = list(filter(None, teams[k]))
+        
+    for l in range(len(teams)):
+        if len(teams[l]) is 11:
+            teams[l] = {
+                'position': teams[l][0],
+                'team': teams[l][1],
+                'points': teams[l][2],
+                'played': teams[l][3],
+                'won': teams[l][4],
+                'drawn': teams[l][5],
+                'lost': teams[l][6],
+                'goals_for': teams[l][7],
+                'goals_against': teams[l][8],
+                'goals_difference': teams[l][9],
+                # 'percentage': teams[l][10],
+                # 'last_games': teams[l][11]
+            }
+        else:
+            teams[l] = {
+                'position': teams[l][0],
+                'team': teams[l][1],
+                'city': teams[l][2],
+                'points': teams[l][3],
+                'played': teams[l][4],
+                'won': teams[l][5],
+                'drawn': teams[l][6],
+                'lost': teams[l][7],
+                'goals_for': teams[l][8],
+                'goals_against': teams[l][9],
+                'goals_difference': teams[l][10],
+                # 'percentage': teams[l][11],
+                # 'last_games': teams[l][12]
+            }
+    tabela['tabela'] = teams
+    jsonify = json.dumps(tabela, indent=4)
+    
+    create_file(str(jsonify), f'tabela_{today()}', 'json')
+    return tabela
         
 
 def utils_date(date: str) -> str:
--- data/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-import os
-
-conn = os.getenv('POSTGRES_PRISMA_URL')
-
-def get_connection():
-    engine = create_engine(conn)
-    connection = engine.connect()
-    return connection
